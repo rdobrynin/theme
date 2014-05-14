5da09b96880acb30ad872388af0edb2bd13c487a
romdo <roman.d@brilliant-solutions.eu>
2014-05-14 17:21:18 +0300

    added task edit tpl

--- sites/all/themes/brilliant_pr/theme/form/brilliant-pr-task.tpl.php
+++ sites/all/themes/brilliant_pr/theme/form/brilliant-pr-task.tpl.php
@@ -1,10 +1,10 @@
-
 <?php
 $form['opt_time']['date']['#title'] = 'Optimal date/time';
 $form['dead_time']['date']['#title'] = 'Deadline date/time';
+unset($form['dead_time']['time']['date']);
+unset($form['opt_time']['time']['date']);
 hide($form['submit']);
-hide($form['submit_task']);
-hide($form['delete']);
+hide($form['remove']);
 hide($form['cancel']);
 ?>
 
@@ -21,28 +21,10 @@ hide($form['cancel']);
       </div>
       <div class="row">
         <div class="col-lg-2">
-          <span class="field-title"><?php print render($form['description']['#title']); ?></span>
-        </div>
-        <div class="col-lg-10 pull-left"  style="margin-bottom: 20px">
-          <?php print render($form['description']); ?>
-        </div>
-      </div>
-      <div class="division">
-      <div class="row">
-        <div class="col-lg-2">
-          <span class="field-title"><?php print render($form['company']['#title']); ?>&nbsp;<span class="required-note">*</span></span>
+          <span class="field-title"><?php print render($form['ref']['#title']); ?>&nbsp;<span class="required-note">*</span></span>
         </div>
         <div class="col-lg-4 pull-left">
-          <?php print render($form['company']); ?>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-lg-2">
-          <span class="field-title"><?php print render($form['customer_name']['#title']); ?>&nbsp;<span class="required-note">*</span></span>
-        </div>
-        <div class="col-lg-4 pull-left">
-          <?php print render($form['customer_name']); ?>
+          <?php print render($form['ref']); ?>
         </div>
       </div>
       <div class="row">
@@ -53,22 +35,31 @@ hide($form['cancel']);
           <?php print render($form['curator']); ?>
         </div>
       </div>
-        <div class="row">
-          <div class="col-lg-2">
-            <button class="btn btn-success" disabled="disabled">Add new contact person</button>
-          </div>
-
+      <div class="row">
+        <div class="col-lg-2">
+          <span class="field-title"><?php print render($form['implementor']['#title']); ?>&nbsp;<span class="required-note">*</span></span>
         </div>
-      </div> <!--   end of division   -->
-
-
+        <div class="col-lg-4 pull-left">
+          <?php print render($form['implementor']); ?>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-2">
+          <span class="field-title"><?php print render($form['description']['#title']); ?></span>
+        </div>
+        <div class="col-lg-10 pull-left"  style="margin-bottom: 20px">
+          <?php print render($form['description']); ?>
+        </div>
+      </div>
       <div class="division">
         <div class="row">
-          <div class="col-lg-3">
+          <div class="col-lg-5">
             <span
               class="field-title"><?php print render($form['opt_time']); ?></span>
           </div>
-          <div class="col-lg-3">
+        </div>
+        <div class="row">
+          <div class="col-lg-5">
             <span
               class="field-title"><?php print render($form['dead_time']); ?></span>
           </div>
@@ -80,8 +71,8 @@ hide($form['cancel']);
       <div class="division">
         <div class="row">
           <div class="col-lg-12 text-right">
-            <span class="field-title"><?php print render($form['submit']); ?><?php print render($form['submit_task']); ?>
-              <?php print render($form['delete']); ?><?php print render($form['cancel']); ?></span>
+            <span class="field-title"><?php print render($form['submit']); ?><?php print render($form['remove']); ?>
+<?php print render($form['cancel']); ?></span>
 
           </div>
 
